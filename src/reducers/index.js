703bf863ff2901ac49deeabd65b6b3dfef51c052
Rubek Joshi <rubek@featherwebs.com>
2019-02-25 22:12:41 +0545

    Fixed empty reducer issue and added assets folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 
 // Import all the reducers created for each part of the store
+import authReducer from './auth';
 
 export default combineReducers({
     // fieldInStore: Reducer
+    auth: authReducer
 });
