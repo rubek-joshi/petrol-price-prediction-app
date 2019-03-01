903454a1c7570ae61048b34e25dfa4d9dedfee06
Rubek Joshi <rubek@featherwebs.com>
2019-03-01 15:21:46 +0545

    Added textfields for both login and sign up

--- src/screens/signin.js
+++ src/screens/signin.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { TextField } from 'react-native-material-textfield';
 
 class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
     render(){
         return (
             <View style={styles.mainContainer}>
+                <TextField
+                    label='Email'
+                    value={this.state.email}
+                    onChangeText={(email) => this.setState({email: email})}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.emailTextInput.focus()}
+                    blurOnSubmit={false}
+                />
+
+                <TextField
+                    label='Password'
+                    value={this.state.password}
+                    onChangeText={(password) => this.setState({password: password})}
+                    ref={(input) => {this.passwordTextInput = input;}}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.confirmPasswordTextInput.focus()}
+                    blurOnSubmit={false}
+                />
+
                 <TouchableOpacity onPress={() => this.props.navigation.navigate('App')}>
                     <Text>Sign In</Text>
                 </TouchableOpacity>
@@ -16,8 +43,6 @@ class SignIn extends Component {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     }
 });
 
--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { TextField } from 'react-native-material-textfield';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullname: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
     render(){
         return (
             <View style={styles.mainContainer}>
-                <Text>Sign Up</Text>
+                <TextField
+                    label='Full Name'
+                    value={this.state.fullname}
+                    onChangeText={(fullname) => this.setState({fullname: fullname})}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.emailTextInput.focus()}
+                    blurOnSubmit={false}/>
+
+                <TextField
+                    label='Email'
+                    value={this.state.email}
+                    onChangeText={(email) => this.setState({email: email})}
+                    ref={(input) => {this.emailTextInput = input;}}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.passwordTextInput.focus()}
+                    blurOnSubmit={false}/>
+
+                <TextField
+                    label='Password'
+                    value={this.state.password}
+                    onChangeText={(password) => this.setState({password: password})}
+                    ref={(input) => {this.passwordTextInput = input;}}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.confirmPasswordTextInput.focus()}
+                    blurOnSubmit={false}/>
+
+                <TextField
+                    label='Confirm Password'
+                    value={this.state.confirmPassword}
+                    onChangeText={(confirmPassword) => this.setState({confirmPassword: confirmPassword})}
+                    ref={(input) => {this.confirmPasswordTextInput = input;}}
+                    returnKeyType={"done"}/>
             </View>
-        )
+        );
     }
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     }
 });
 
