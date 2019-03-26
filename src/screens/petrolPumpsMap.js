5b5dfa09c1806401fb326e4aaf05cf4faa02aa37
Rubek Joshi <rubek@featherwebs.com>
2019-03-27 02:39:41 +0545

    Integrated google maps

--- src/screens/petrolPumpsMap.js
+++ src/screens/petrolPumpsMap.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import MyColors from '../config/colors';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
 
 class Map extends Component {
     static navigationOptions = {
@@ -20,16 +34,20 @@ class Map extends Component {
     render(){
         return (
             <View style={styles.mainContainer}>
-                <Text>Map goes here</Text>
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                        latitude: 27.707716,
+                        longitude: 85.325121,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                    >
+                </MapView>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1
-    }
-});
 
 export default Map;
