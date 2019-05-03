d3f76c266e959bbed26f2efa320caeb6d899091e
Rubek Joshi <rubek@featherwebs.com>
2019-05-03 20:45:18 +0545

    Optimized code

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, View, Text} from 'react-native';
 
 import MyColors from '../config/colors';
 import MyFont from '../config/header';
