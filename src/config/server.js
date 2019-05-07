d88470ba320ca9324af9e85b9501bde2d8dabfa4
Rubek Joshi <rubek@featherwebs.com>
2019-05-07 15:07:53 +0545

    USB Tethering IP

--- src/config/server.js
+++ src/config/server.js
@@ -1,3 +1,3 @@
-export const ServerIp = "http://192.168.1.65:3000";
+export const ServerIp = "http://192.168.42.88:3000";
 export const GasStationsIp =
   "https://maps.googleapis.com/maps/api/place/nearbysearch";
